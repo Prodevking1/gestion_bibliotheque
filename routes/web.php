2cf900c1e44d76a1db03ad43d5adcbe7057fb58d
Murray Hat <77835100+Prodevking1@users.noreply.github.com>
2022-06-22 20:17:24 +0000

    etudiant routes added

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('register');
+
+Route::get('/etudiants', [EtudiantsController::class, 'index'])->name('etudiants');
+Route::get('/etudiants/create', [EtudiantsController::class, 'create'])->name('etudiants.create');
+Route::get('/etudiants/edit/{id}', [EtudiantsController::class, 'edit'])->name('etudiants.edit');
+Route::post('/etudiants/update/{id}', [EtudiantsController::class, 'update'])->name('etudiants.update');
+Route::post('/etudiants/delete/{id}', [EtudiantsController::class, 'destroy'])->name('etudiants.destroy');
+Route::post('/etudiants/store/', [EtudiantsController::class, 'store'])->name('etudiants.store');
