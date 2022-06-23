1a0b4e5579e8f2ae68e5d95bd3594e1b38ebb406
NasserODG <nasserodg60@gmail.com>
2022-06-23 18:01:44 +0000

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         @csrf
 
                         <div class="row mb-3" style=" font-family: Stencil Std, fantasy; font-size: 150%;">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Adresse') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
