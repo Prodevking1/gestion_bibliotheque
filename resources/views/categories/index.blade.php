37ca399b5727b108e2f1d1930075b31e054d487a
NasserODG <nasserodg60@gmail.com>
2022-06-28 19:52:31 +0000

    Mise en forme catégories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,71 +3,70 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid py-4" style="min-height: 700px;" width=100%>
-			<h1>categories</h1>
-			<ol class="breadcrumb mt-4 mb-4 bg-light p-2 border">
-				<li class="breadcrumb-item"><a href="index.php">Tableau de bord</a></li>
-				<li class="breadcrumb-item active">Gestion des utilisateurs</li>
-			</ol>
-      <div class="" style="">
-            <a href="{{ route('categories.create') }}">
-            <button type="submit" class="btn btn-outline-info" style=" font-family: Stencil Std, fantasy; font-size: 100%;border-color:none;color:;">
-                      {{ __('Ajoutez un Etudiant') }}
-                </button>
-            </a>
-          </div>
+	<h1>categories</h1>
+		<ol class="breadcrumb mt-4 mb-4 bg-light p-2 border">
+			<li class="breadcrumb-item"><a href="index.php">Tableau de bord</a></li>
+			<li class="breadcrumb-item active">Catégories</li>
+		</ol>
+      <div class="form-group">
+        <a href="{{ route('categories.create') }}">
+          <button type="submit" class="btn btn-primary" style=" font-family: Lato; font-size: 120%;border-color:none;">
+            {{ __('Ajoutez') }}
+          </button>
+        </a>
+      </div>
           <br>
 			<div class="card mb-4">
-				<table class="table align-middle mb-0 bg-white">
-  <thead class="bg-light" width=100%>
-    <tr width=100%>
-    
-      <th width=25%>ID</th>
-      <th width=40%>Titre</th>
-      <th>Actions</th>
-     
-    </tr>
-  </thead>
-  @forelse($categories as $categories)
-  <tbody>
-    <tr>
-      <td>
-        <div class="d-flex align-items-center">
-          <img
-              src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-              alt=""
-              style="width: 45px; height: 45px"
-              class="rounded-circle"
-              />
-          <div class="ms-3">
-            <th width=25%><p class="fw-bold mb-1">{{ $categories->id }}</p></th>
-            <th width=40%><p class="text-muted mb-0">{{$categories->titre}}</p></th>
-        
-           
-          </div>
-        </div>
-      </td>
-     
+			  <table class="table align-middle mb-0 bg-white">
+          <thead class="bg-light" width=100%>
+            <tr width=100%>
+            
+              <th width=25%>ID</th>
+              <th width=40%>Titre</th>
+              <th>Actions</th>
+            
+            </tr>
+          </thead>
+            @forelse($categories as $categories)
+          <tbody>
+            <tr>
+              <td>
+                <div class="d-flex align-items-center">
+                  <img
+                      src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                      alt=""
+                      style="width: 45px; height: 45px"
+                      class="rounded-circle"
+                      />
+                  <div class="ms-3">
+                    <th width=25%><p class="fw-bold mb-1">{{ $categories->id }}</p></th>
+                    <th width=40%><p class="text-muted mb-0">{{$categories->titre}}</p></th>
+                
+                  
+                  </div>
+                </div>
+              </td>
+            
 
-                             <td>
-                                            <a href="{{ url('/categories/' . $categories->id) }}" title="View categorie"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/categories/' . $categories->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
- 
-                                            <form method="POST" action="{{ url('/categories' . '/' . $categories->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete categories" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-      
-    </tr>
-    @empty
-     <tr>
-         <td colspan="8">Aucun etudiant n'a ete trouve !</td>
-     </tr>
-  </tbody>
-  @endforelse
-</table>
-			</div>
+              <td>
+                <a href="{{ url('/categories/' . $categories->id) }}" title="View categorie"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                <a href="{{ url('/categories/' . $categories->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+        
+                  <form method="POST" action="{{ url('/categories' . '/' . $categories->id) }}" accept-charset="UTF-8" style="display:inline">
+                      {{ method_field('DELETE') }}
+                      {{ csrf_field() }}
+                    <button type="submit" class="btn btn-danger btn-sm" title="Delete categories" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                  </form>
+              </td>
+            </tr>
+              
+            @empty
+            <tr>
+                <td colspan="8">Aucun etudiant n'a ete trouve !</td>
+            </tr>
+          </tbody>
+            @endforelse
+        </table>
+		  </div>
 		</div>
 @endsection
