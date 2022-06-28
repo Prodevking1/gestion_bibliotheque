32efbcc523675725b7d54af3116df5ba846c71c2
bkt <saintpatrickt31@gmail.com>
2022-06-28 11:59:48 +0200

    reff

--- resources/views/etudiants/index.blade.php
+++ resources/views/etudiants/index.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.app')
 @section('content')
-<div class="container-fluid py-4" style="min-height: 700px;">
-			<h1>Gestion des utilisateurs</h1>
-			<ol class="breadcrumb mt-4 mb-4 bg-light p-2 border">
+<div class="container-fluid py-4">
+			<h1 >Gestion des Etudiants</h1>
+			<ol class="breadcrumb mt-4 mb-4 bg-light p-2 border" >
 				<li class="breadcrumb-item"><a href="index.php">Tableau de bord</a></li>
-				<li class="breadcrumb-item active">Gestion des utilisateurs</li>
+				<li class="breadcrumb-item active">Gestion des Etudiants</li>
 			</ol>
+          <div class="" style="">
+            <a href="{{ route('etudiants.create') }}">
+            <button type="submit" class="btn btn-outline-primary" style=" font-family: Stencil Std, fantasy; font-size: 100%;border-color:black;color:black;">
+                      {{ __('Ajoutez un Etudiant') }}
+                </button>
+            </a>
+          </div>
+                        
+        <div class="search"style="margin-left:80%;">
+            <form class="searchbox" method="get" action="search" autocomplete="off">
+                  <input name="q" type= "text" size="15" placeholder="Rechercher un Etudiant" />
+                  <input class="button-submit" type= "submit" value="" />
+            </form>
+           
+        </div>
+
 
 			<div class="card mb-4">
 				<table class="table align-middle mb-0 bg-white">
@@ -44,9 +60,14 @@
       </td>
       <td>Neant</td>
       <td>
-        <button type="button" class="btn btn-link btn-sm btn-rounded">
-          Editer
-        </button>
+      <div class="btn btn-link btn-sm btn-rounded" >
+        <li>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M160 400C160 408.8 152.8 416 144 416C135.2 416 128 408.8 128 400V192C128 183.2 135.2 176 144 176C152.8 176 160 183.2 160 192V400zM240 400C240 408.8 232.8 416 224 416C215.2 416 208 408.8 208 400V192C208 183.2 215.2 176 224 176C232.8 176 240 183.2 240 192V400zM320 400C320 408.8 312.8 416 304 416C295.2 416 288 408.8 288 400V192C288 183.2 295.2 176 304 176C312.8 176 320 183.2 320 192V400zM317.5 24.94L354.2 80H424C437.3 80 448 90.75 448 104C448 117.3 437.3 128 424 128H416V432C416 476.2 380.2 512 336 512H112C67.82 512 32 476.2 32 432V128H24C10.75 128 0 117.3 0 104C0 90.75 10.75 80 24 80H93.82L130.5 24.94C140.9 9.357 158.4 0 177.1 0H270.9C289.6 0 307.1 9.358 317.5 24.94H317.5zM151.5 80H296.5L277.5 51.56C276 49.34 273.5 48 270.9 48H177.1C174.5 48 171.1 49.34 170.5 51.56L151.5 80zM80 432C80 449.7 94.33 464 112 464H336C353.7 464 368 449.7 368 432V128H80V432z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M160 256C160 185.3 217.3 128 288 128C358.7 128 416 185.3 416 256C416 326.7 358.7 384 288 384C217.3 384 160 326.7 160 256zM288 336C332.2 336 368 300.2 368 256C368 211.8 332.2 176 288 176C287.3 176 286.7 176 285.1 176C287.3 181.1 288 186.5 288 192C288 227.3 259.3 256 224 256C218.5 256 213.1 255.3 208 253.1C208 254.7 208 255.3 208 255.1C208 300.2 243.8 336 288 336L288 336zM95.42 112.6C142.5 68.84 207.2 32 288 32C368.8 32 433.5 68.84 480.6 112.6C527.4 156 558.7 207.1 573.5 243.7C576.8 251.6 576.8 260.4 573.5 268.3C558.7 304 527.4 355.1 480.6 399.4C433.5 443.2 368.8 480 288 480C207.2 480 142.5 443.2 95.42 399.4C48.62 355.1 17.34 304 2.461 268.3C-.8205 260.4-.8205 251.6 2.461 243.7C17.34 207.1 48.62 156 95.42 112.6V112.6zM288 80C222.8 80 169.2 109.6 128.1 147.7C89.6 183.5 63.02 225.1 49.44 256C63.02 286 89.6 328.5 128.1 364.3C169.2 402.4 222.8 432 288 432C353.2 432 406.8 402.4 447.9 364.3C486.4 328.5 512.1 286 526.6 256C512.1 225.1 486.4 183.5 447.9 147.7C406.8 109.6 353.2 80 288 80V80z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
+
+        </li>
+      </div>
       </td>
     </tr>
     @empty
@@ -59,3 +80,55 @@
 			</div>
 		</div>
 @endsection
+<style>
+            .searchbox {
+                background: url(http://3.bp.blogspot.com/-g-zH25_DoxI/VD1BuatkgII/AAAAAAAAAgc/00hxspfvv3s/s1600/searchbar.png) no-repeat;
+                width: 208px;
+                height: 29px;
+                
+            }
+            input:focus::-webkit-input-placeholder {
+                color: transparent;
+            }
+            input:focus:-moz-placeholder {
+                color: transparent;
+            }
+            input:focus::-moz-placeholder {
+                color: transparent;
+            }
+            .searchbox input {
+                outline: none;
+            }
+            .searchbox input[type="text"] {
+                background: transparent;
+                margin: 3px 0px 0px 20px;
+                padding: 5px 0px 5px 0px;
+                border-width: 0px;
+                font-family: Arial Narrow, Arial, sans-serif;
+                font-size: 12px;
+                color: #828282;
+                width: 70%;
+                display: inline-table;
+                vertical-align: top;
+            }
+            .button-submit {
+                background: url(http://4.bp.blogspot.com/-OcDQ6Z9ojlQ/VD1KnwJjFOI/AAAAAAAAAgs/cu_pKN6bpL8/s1600/magnifier.png) no-repeat;
+                border-width: 0px;
+                cursor: pointer;
+                margin-left: 10px;
+                margin-top: 4px;
+                width: 21px;
+                height: 22px;
+            }
+            .button-submit:hover {
+                background: url(http://2.bp.blogspot.com/-4-xFDFGKJrA/VD1Kn04f9TI/AAAAAAAAAgw/2ta84QY1x9A/s1600/magnifier-hover.png) no-repeat;
+            }
+            .button-submit:active {
+                background: url(http://2.bp.blogspot.com/-4-xFDFGKJrA/VD1Kn04f9TI/AAAAAAAAAgw/2ta84QY1x9A/s1600/magnifier-hover.png) no-repeat;
+                outline: none;
+            }
+            .button-submit::-moz-focus-inner {
+                border: 0;
+            }
+            </style>
+
