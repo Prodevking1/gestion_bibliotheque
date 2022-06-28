90a792f21fe6b66de233c87d57c41aa97f1e0782
bkt <saintpatrickt31@gmail.com>
2022-06-28 14:10:49 +0200

    rreeee

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,9 +57,9 @@
 
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.php">Bibliothèque</a>
+        <a class="navbar-brand ps-3" style="color:rgb(0,191,255); font-family: Stencil Std, fantasy;" href="index.php">Bibliothèque</a>
         <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"  id="sidebarToggle" href="#!"><i
                 class="fas fa-bars"></i></button>
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
 
@@ -68,7 +68,7 @@
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                    data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw" style="color:rgb(0,191,255);"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="profile.php">Profil</a></li>
                     <li><a class="dropdown-item" href="setting.php">Reglages</a></li>
