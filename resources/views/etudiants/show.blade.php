f79522ce07145261e214ec7619345a6bc00e9478
bkt <saintpatrickt31@gmail.com>
2022-06-29 17:09:30 +0200

    III

--- resources/views/etudiants/show.blade.php
+++ resources/views/etudiants/show.blade.php
@@ -20,35 +20,35 @@
 					<!-- Input de nom de l'etudiant -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Nom</label>
-						<input type="text" name="nom" id="nom" class="form-control" placeholder="OUEDRAOGO" required/>
+						<input type="text" name="nom" id="nom" class="form-control" value="{{$etudiants->nom}}" placeholder="OUEDRAOGO" disabled/>
 					</div>
 
 
 					<!-- Input de prenom -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Prenom</label>
-						<input type="text" name="prenom" id="prenom" class="form-control" placeholder="Nasser" required/>
+						<input type="text" name="prenom" id="prenom" class="form-control" value="{{$etudiants->prenom}}" placeholder="Nasser" disabled/>
 					</div>
 
 
 					<!-- Date de naissance -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Date de Naissance</label>
-						<input type="date" name="age" id="age" class="form-control" placeholder="jj/mm/aaaa" required/>
+						<input type="date" name="age" id="age" class="form-control" value="{{$etudiants->age}}" placeholder="jj/mm/aaaa" disabled/>
 					</div>
 
 
 					<!-- Email -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Email</label>
-						<input type="email" name="email" id="email" class="form-control" placeholder="nasserodg60@gmail.com" required/>
+						<input type="email" name="email" id="email" class="form-control" value="{{$etudiants->email}}" placeholder="nasserodg60@gmail.com" disabled/>
 					</div>
 
 
 					<!-- Téléphone -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Téléphone</label>
-						<input type="phone" name="tel" id="tel" class="form-control" placeholder="Ex: +226 60558128" required/>
+						<input type="phone" name="tel" id="tel" class="form-control" value="{{$etudiants->tel}}" placeholder="Ex: +226 60558128" disabled/>
 					</div>
 
 
@@ -57,11 +57,11 @@
 						<label class="form-label" style="font-weight: bold;">Genre</label>
 						<div class="col-md-12 mx-auto" >
 							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="genre" id="masculinChoice" value="masculin" checked>
+								<input class="form-check-input" type="radio" name="genre" id="masculinChoice" value="{{$etudiants->genre}}" value="masculin" disabled>
 								<label class="form-check-label" for="inlineRadio1">Masculin</label>
 							</div>
 							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="genre" id="femininChoice" value="feminin">
+								<input class="form-check-input" type="radio" name="genre" id="femininChoice" value="{{$etudiants->genre}}" value="feminin"  disabled>
 								<label class="form-check-label" for="inlineRadio2">Feminin</label>
 							</div>
 						</div>
@@ -72,8 +72,8 @@
 					<!-- Filière with select option -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Filière</label>
-						<select class="form-select" name="filiere" aria-label="Default select example" required>
-							<option value="" selected disabled>Selectionnez votre filière</option>
+						<select class="form-select" name="filiere" value="{{$etudiants->filiere}}" aria-label="Default select example" disabled>
+							<option value="" selected disabledd>Selectionnez votre filière</option>
 							<option value="MIAGE">MIAGE</option>
 							<option value="ADB">ADB</option>
 							<option value="CCA">CCA</option>
@@ -86,8 +86,8 @@
 					<!-- Niveau d'étude with select option -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Niveau</label>
-						<select class="form-select" name="niveau" aria-label="Default select example" required>
-							<option value="" selected disabled>Selectionnez votre niveau</option>
+						<select class="form-select" name="niveau" value="{{$etudiants->niveau}}" aria-label="Default select example" disabled>
+							<option value="" selected disabledd>Selectionnez votre niveau</option>
 							<option value="LICENCE 1">LICENCE 1</option>
 							<option value="LICENCE 2">LICENCE 2</option>
 							<option value="LICENCE 3">LICENCE 3</option>
@@ -98,14 +98,14 @@
 					<!-- Adresse de l'étudiant -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Adresse</label>
-						<input type="text" name="adresse" id="adresse" class="form-control" placeholder="Ex: Saaba" required/>
+						<input type="text" name="adresse" id="adresse" class="form-control"  value="{{$etudiants->adresse}}" placeholder="Ex: Saaba" disabled/>
 					</div>
 
 
 					<!-- Mot de passe etudiant -->
 					<div class="col-md-6">
 						<label class="form-label" style="font-weight: bold;">Mot de passe</label>
-						<input type="password" name="user_password" id="user_password" class="form-control" placeholder="Entrez votre mot de passe" required/>
+						<input type="password" name="user_password" id="user_password" class="form-control" value="{{$etudiants->user_password}}" placeholder="Entrez votre mot de passe" disabled/>
 					</div>
 
 					<br>
@@ -113,7 +113,7 @@
 					<!-- Upload photo etudiant -->
 					<div class="mb-3">
 						<label class="form-label" style="font-weight: bold;">Photo</label><br />
-						<input type="file" name="user_profile" id="user_profile" required/>
+						<input type="file" name="user_profile"  value="{{$etudiants->user_profile}}" id="user_profile" disabled/>
 						<br />
 						<span class="text-muted">Seules les images .jpg & .png sont autorisées. La taille doit être égale à 225 x 225</span>
 					</div>
