254acb9435145d50dee031e38e4a735768e1b24a
bkt <saintpatrickt31@gmail.com>
2022-07-01 22:25:41 +0200

    t

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.app')
 @section('content')
  
- 
-<div class="card">
-  <div class="card-header">Liste des categories</div>
-  <div class="card-body">
-   
- 
-        <div class="card-body">
-        <h5 class="card-title">Titre : {{ $categories->titre }}</h5>
-  </div>
-       
-    </hr>
-  
-  </div>
-</div>
+<div class="container" style="margin-top: 2%;text-align:center; font-family:Lato;">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+    <h1 style="font-family:Lato;">Categories</h1>
+			            <div class="card-body"  style="text-transform : uppercase; font-family: Lato;">
+                    <table class="table align-middle mb-0 bg-white">
+                      <thead class="bg-light">
+                        <tr>
+                        <th scope="col">ID</th> 
+                          <th scope="col">Titre</th>
+                          <th scope="col">Emplacement</th>                        
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                            <div class="d-flex align-items-center">
+                              <!-- <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 45px; height: 45px;" class="rounded-circle" /> -->
+                              <div class="fixed-left">
+                              <th scope="row" >{{ $categories->id }}</th>
+                                <th scope="row" width=20%>{{$categories->titre}}</th>
+                              </div>
+                 </div>
 @endsection
