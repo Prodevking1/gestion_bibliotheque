a146bc643870076f211c4ab8bcdbd18fe76ac4f2
Murray Hat <77835100+Prodevking1@users.noreply.github.com>
2022-07-12 11:05:01 +0000

    pre final

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//import all controllers from
+use App\Http\Controllers\Controller;
+use App\Models\Etudiants;
+use App\Models\Livres;
+use App\Models\Emprunts;
+use App\Models\Reservations;
+use App\Models\Auteurs;
 
 class HomeController extends Controller
 {
@@ -23,6 +30,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // cunt all items in the
+        return view('home', 
+        [
+        'livres' => Livres::count(),
+        'reservations' => Reservations::count(),
+        'etudiants' => Etudiants::count(),
+        'emprunts' => Emprunts::count(),
+        'auteurs' => Auteurs::count(),
+        ]);
     }
 }
